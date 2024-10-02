54990f0ad800a4041df2a64a1bc8a3019edcaefa
Starskii <jakeylakie@yahoo.com>
2024-10-01 23:45:22 -0700

    started win check logic

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const game = (function (){
-    const COMPLETED = 0;
-    const INCOMPLETE = -1;
+    const TRANSACTION_STATUS = Object.freeze({
+        SUCCESS: 0,
+        FAILURE: 1
+    });
+
+    const GAME_STATE = Object.freeze({
+        IN_PROGRESS: 0,
+        WIN: 1,
+        DRAW: 2
+    });
+
+    const BOARD_STATE = Object.freeze({
+        EMPTY: 0,
+        PLAYER_1: 1,
+        PLAYER_2: 2
+    });
 
 
     let gameBoard = [
@@ -12,8 +26,28 @@ const game = (function (){
     let playerTurn = true;
     let playerName = "Player 1";
     let opponentName = "Player 2";
+    let winningPlayer = BOARD_STATE
+    let turnCount = 0;
 
-    let gameStatus = 0;
+    let gameStatus = GAME_STATE.IN_PROGRESS;
+
+    function updateGameStatus(){
+        turnCount++;
+        checkStatus();
+    }
+
+    function checkEquivalence(firstNumber, secondNumber, thirdNumber){
+        return firstNumber == secondNumber && firstNumber == thirdNumber;
+    }
+
+    function checkStatus(){
+        //Check rows
+        gameBoard.forEach((row) => {
+            if (checkEquivalence(row[0], row[1], row[2])){
+                
+            }
+        })
+    }
 
     function getPlayerName(){
         return playerName;
@@ -32,17 +66,20 @@ const game = (function (){
     }
 
     function userAction(xCoordinate, yCoordinate){
-        const playerValue = playerTurn ? 1 : -1;
-        if (gameBoard[yCoordinate][xCoordinate] != 0)
-            return INCOMPLETE;
+        const playerValue = playerTurn ? BOARD_STATE.PLAYER_1 : BOARD_STATE.PLAYER_2;
+        
+        if (gameBoard[yCoordinate][xCoordinate] != BOARD_STATE.EMPTY)
+            return TRANSACTION_STATUS.FAILURE;
+        if (gameStatus != GAME_STATE.IN_PROGRESS)
+            return TRANSACTION_STATUS.FAILURE;
         gameBoard[yCoordinate][xCoordinate] = playerValue;
         playerTurn = !playerTurn;
-        checkWin();
-        return COMPLETED;
+        updateGameStatus();
+        return TRANSACTION_STATUS.SUCCESS;
     }
 
     function getCurrentPlayer(){
-        return playerTurn ? playerName : opponentName;
+        return playerTurn ? BOARD_STATE.PLAYER_1 : BOARD_STATE.PLAYER_2;
     }
 
     function logBoard(){
@@ -66,17 +103,20 @@ const game = (function (){
         resetGame,
         userAction,
         logBoard,
-        getCurrentPlayer
+        getCurrentPlayer,
+        BOARD_STATE,
+        GAME_STATE,
+        TRANSACTION_STATUS
     }
 })();
 
-console.log(game.getCurrentPlayer());
-console.log(game.userAction(1, 0));
-console.log(game.getCurrentPlayer());
-console.log(game.userAction(1, 0));
-console.log(game.getCurrentPlayer());
-console.log(game.userAction(1, 1));
-console.log(game.getCurrentPlayer());
+console.log(`Current Player: ${game.getCurrentPlayer()}`);
+console.log(`User Action Response: ${game.userAction(1, 0)}`);
+console.log(`Current Player: ${game.getCurrentPlayer()}`);
+console.log(`User Action Response: ${game.userAction(1, 0)}`);
+console.log(`Current Player: ${game.getCurrentPlayer()}`);
+console.log(`User Action Response: ${game.userAction(1, 1)}`);
+console.log(`Current Player: ${game.getCurrentPlayer()}`);
 game.logBoard();
 game.resetGame();
 game.logBoard();
